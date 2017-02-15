c8f7f6324b5f6a6283d2412b8e670626614d1a19
lufinkey <luisfinke@gmail.com>
2017-02-14 20:00:32 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]
+then
+	>&2 echo "This script must be run as root"
+	exit 1
+fi
+
+server_home="/var/minecraft"
+
+while [ "$agree" != "y" ] && [ "$agree" != "n" ]
+do
+	read -p "Do you agree to the Minecraft EULA? (https://account.mojang.com/documents/minecraft_eula) (y/n): " agree
+done
+if [ "$agree" == "n" ] || [ "$agree" != "y" ]
+then
+	exit 1
+fi
+
+# Moving into script directory
+base_dir=$(dirname "${BASH_SOURCE[0]}")
+cd "$base_dir"
+
+if type "apt-get"
+then
+	apt-get install -y $(cat dependencies.txt)
+elif type "yum"
+then
+	yum install -y $(cat dependencies.txt)
+else
+	echo "Could not determine your package manager"
+	echo "You'll have to install dependencies manually"
+	echo "dependencies are listed in dependencies.txt"
+fi
+
+if [ -z "$(id -u minecraft)" ]
+then
+	useradd -r -s "/bin/false" minecraft
+else
+	echo 'user "minecraft" already exists'
+fi
+mkdir -p "$server_home"
+mkdir -p "$server_home/servers"
+cp "minecraft-server" "/usr/local/bin/minecraft-server"
+cp "minecraft-server.service" "/etc/init.d/minecraft-server"
+chmod +x "/etc/init.d/minecraft-server"
+chmod +x "/usr/local/bin/minecraft-server"
+chown -R minecraft:minecraft "$server_home"
+
+minecraft-server update
+
+update-rc.d minecraft-server defaults
+
